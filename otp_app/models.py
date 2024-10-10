a62c7329f7eb3ae36a876f4cf2ca191d2f3c7e71
satyam_45 <satyampokharna45@gmail.com>
2024-10-10 15:18:10 +0530

    model:create table student

--- otp_app/models.py
+++ otp_app/models.py
@@ -1,3 +1,16 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    enrollment_no = models.CharField(max_length=20, unique=True)
+    phone_no = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.enrollment_no
+
+class Device(models.Model):
+    enrollment_no = models.OneToOneField(Student, on_delete=models.CASCADE)
+    device_id = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.device_id
